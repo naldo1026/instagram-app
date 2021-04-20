1cb74527fffeb33458ec5b897181e99805051589
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-04-20 22:09:52 +0100

    show page and edit, update, destroy methods addded

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,6 +2,7 @@ class PicsController < ApplicationController
   before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pics = Pic.all.order('created_at DESC')
   end
 
   def show
@@ -20,6 +21,23 @@ class PicsController < ApplicationController
       render 'new'
     end
 
+  end
+
+  def edit
+  
+  end
+
+  def update
+    if @pic.update(pic_params) 
+      redirect_to @pic, notice: 'Congrats'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
   end
 
   private
